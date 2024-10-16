7dd8d52af45dda95bb7daae0762cbe2f35010053
HJMillan <hjs.millan@gmail.com>
2024-10-16 18:38:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Notification form handling
+document.getElementById("notifyForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Gracias por suscribirte. Te notificaremos cuando lancemos la página.");
+});
