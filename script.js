ed58c1b0fde2ab65c61fdef127c9e2ed6c77d961
HJMillan <hjs.millan@gmail.com>
2024-11-09 20:53:18 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Notification form handling
-document.getElementById("notifyForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Gracias por suscribirte. Te notificaremos cuando lancemos la página.");
-});
